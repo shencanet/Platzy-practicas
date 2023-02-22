8feecf237d22027358f75d5353343d59ea1222e7
shen <santi.dionis@gmail.com>
2023-02-22 20:41:11 +0100

    ADD: CLASS

--- .vscode/Array.js
+++ .vscode/Array.js
@@ -1,1 +1,13 @@
 // const array = ["Diego", "Karen", "Oscar"];
+
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {
+
+        }
+    }
+    get(index){
+        return this.data[index];
+    }
+}
