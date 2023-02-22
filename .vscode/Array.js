bf7082f5b2e06aa45a635f58a8c49fa0177d1a20
shen <santi.dionis@gmail.com>
2023-02-22 21:29:56 +0100

    ADD: Array

--- .vscode/Array.js
+++ .vscode/Array.js
@@ -3,11 +3,49 @@
 class MyArray {
     constructor(){
         this.length = 0;
-        this.data = {
-
-        }
+        this.data = {};
     }
     get(index){
         return this.data[index];
     }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+    Pop() {
+        const lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+
+    }
+
+    delete(index){
+        const item = this.data[index]
+        this.shiftIndex(index);
+        return item;
+    }
+
+       shiftIndex(index){
+        for (let i = index; i< this.lenght -1; i++ ){
+            this.data[i] = this.data[i + 1 ];
+
+           }
+            delete this.data[this.length -1 ];
+            this.length--; 
+            return this.data;
+       }
+
+    
 }
+const myArray = new MyArray();
+myArray.push("Jose");
+myArray.push("Adrian");
+myArray.push("Santi");
+console.log(myArray)
+//myArray.get(1);
+//myArray.Pop();
+myArray.delete(0);
+console.log(myArray)
+
