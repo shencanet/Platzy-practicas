e7352cf32e6c24b507e72ea0493ef6bc733dab19
shen <santi.dionis@gmail.com>
2023-02-23 20:27:26 +0100

    ADD:set

--- .vscode/hashTables.js
+++ .vscode/hashTables.js
@@ -13,7 +13,30 @@ hashMethod(key) {
     return hash;
     
 }
+set(key, value) {
+    const address = this.hashMethod(key);
+    if(!this.data[address]){
+        this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+}
+set(key){
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+    if(currentBucket){
+        for(let i =0; i<currentBucket.lenght; i++){
+            if(currentBucket[i][0] === key){
+                return currentBucket[i][1];
+            }
+        }
+    }
+
+}
 }
 
-const myHashTable = new HashTable(25);
-console.log(myHashTable);
+const myHashTable = new HashTable(50);
+
+myHashTable.set("Diego", 1990);
+myHashTable.set("Alejandro", 1998);
+
