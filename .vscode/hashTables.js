5bbccb78757ca6dbe181d20816253defe46a0517
shen <santi.dionis@gmail.com>
2023-02-23 20:33:44 +0100

    ADD: Delete

--- .vscode/hashTables.js
+++ .vscode/hashTables.js
@@ -41,6 +41,35 @@ getAllKeys(){
     
     return items;
   }
+  delete(key){
+
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+    let itemDeleted
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          const index = i
+          itemDeleted = currentBucket[i]
+          if (currentBucket.length > 1) {
+            for (let i = index; i < currentBucket.length - 1; i++) {
+              this.data[address][0] = this.data[address][i + 1];
+            }
+            delete this.data[address][currentBucket.length - 1]
+            console.log("Se ha eliminado el elemento");
+            return itemDeleted
+          } else {
+            delete this.data[address];
+            console.log("Se ha liberado el Address");
+            return itemDeleted
+          }
+        }
+      }
+    }
+    console.log("No se encontro ningun elemento a borrar");
+    return undefined
+  }
+}
 
 }
 
