cc32a7b75ba7bf93ef249b23ced300fdcffba454
shen <santi.dionis@gmail.com>
2023-02-23 20:58:20 +0100

    ADD: Linked List

---
+++ LinkedList/singly.js
@@ -0,0 +1,48 @@
+/*
+Linked List son simplemente un conjunto de nodos 
+ordenados que contienen los valores que necesitemos
+ (numbers, strings, boolean, etc). 
+ Cada uno tiene un valor y una referencia a un 
+ siguiente nodo.
+
+   Prepend ⇒ Agregar un nodo al inicio
+    Append ⇒ Agregar un nodo al final
+    Lookup / Search ⇒ Buscar un nodo
+    Insert ⇒ insertar un nodo en la lista
+    Delete ⇒ Borrar un nodo
+
+ */
+let singlyLinkedList = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null,
+        },
+      },
+    },
+  },
+};
+class Node {
+   constructor(value) {
+      this.value;
+      this.next = null;
+   }
+}
+
+class MysinglyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let MysinglyLinkedList = new MysinglyLinkedList(1);
