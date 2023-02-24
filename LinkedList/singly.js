6531063900bd70260a5c37144c92d89c805d50ef
shen <santi.dionis@gmail.com>
2023-02-24 20:49:20 +0100

    ADD: append prepend

--- LinkedList/singly.js
+++ LinkedList/singly.js
@@ -12,51 +12,87 @@ ordenados que contienen los valores que necesitemos
     Delete ⇒ Borrar un nodo
 
  */
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null,
-        },
-      },
-    },
-  },
-};
+// 0 --> 1 -- > 2 -- > 3-- > 4-- > 5 -- > 6 --> null
+0;
+// let singlyLinkedList = {
+//   head: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: null,
+//         },
+//       },
+//     },
+//   },
+// };
+
 class Node {
-   constructor(value) {
-      this.value;
-      this.next = null;
-   }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-class MysinglyLinkedList {
+class MySinglyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
       next: null,
     };
     this.tail = this.head;
+
     this.length = 1;
   }
-}
-append(value) {
-  const newNode = new Node(value);
-  this.tail.next = newNode;
-  this.tail = newNode;
-  this.length++;
-  return this;
+  append(value) {
+    const newNode = new Node(value);
 
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.length++;
+
+    return this;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+
+    this.length++;
+
+    return this;
+  }
+
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
 }
-prepend (value) {
-  const newNode = new Node(value);
-  newNode.next = this.head;
-  this.head = newNode;
-  this.length++;
-  return this;
-}
-let MysinglyLinkedList = new MysinglyLinkedList(1);
+
+let myLinkedList = new MySinglyLinkedList(1);
