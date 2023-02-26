032808b0eac16b82fa06ac736f248c6cfb714f9e
shen <santi.dionis@gmail.com>
2023-02-26 19:41:02 +0100

    ADD: GRAPH 2

--- Graphs/Graphs_2.js
+++ Graphs/Graphs_2.js
@@ -14,3 +14,16 @@ class Graph {
   }
   
   const myGraph = new Graph();
+
+  myGraph.addVertex("1");//crea indice
+  myGraph.addVertex("3");
+  myGraph.addVertex("4");
+  myGraph.addVertex("1");
+  myGraph.addVertex("5");
+  myGraph.addVertex("6");
+  myGraph.addVertex("8");
+  console.log(myGraph); 
+
+  addEdge("", "");
+  addEdge("", "");
+  addEdge("", "");
