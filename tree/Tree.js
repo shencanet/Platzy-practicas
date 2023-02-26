6e20437ec03772f825e19eb2f0f2a829b343214e
shen <santi.dionis@gmail.com>
2023-02-26 18:15:50 +0100

    ADD: Tree

--- tree/Tree.js
+++ tree/Tree.js
@@ -1,6 +1,6 @@
-//      10
-//   4     20
-// 2  8  17  170
+//          [10] // PADRE
+//    [4]         [20] //nodo
+// [2]  [8]    [17]  [170] //HIJOS
 
 class Node {
     constructor(value) {
@@ -12,23 +12,26 @@ class Node {
   
   class BinarySearchTree {
     constructor() {
-      this.root = null;
+      this.root = null; //NODO PADRE 1 NODO
     }
     insert(value) {
-      const newNode = new Node(value);
-      if (this.root === null) {
-        this.root = newNode;
-      } else {
-        let currentNode = this.root;
+      const newNode = new Node(value); //NODO A INSERTAR
+      if (this.root === null) {//SI YA EXISTE NODO CREA HIJOS SI NO SE CONVIERTE NODO PADRE
+
+        this.root = newNode;// CREA EL NODO PADRE ROOT
+
+      } else {// YA EXISTE ROOT CREA HIJOS
+        let currentNode = this.root;//RECORRE ARBOL
         while (true) {
           if (value < currentNode.value) {
-            if (!currentNode.left) {
-              currentNode.left = newNode;
+            if (!currentNode.left) {//LADO IZQUIERDO HIJO DECREMENTA VALOR
+              currentNode.left = newNode;//VALIDA QUE NO EXISTA NODO LADO IZQUIEDO
               return this;
             }
             currentNode = currentNode.left;
-          } else {
-            if (!currentNode.right) {
+          } else {//LADO DERECHO HIJO INCREMENTA VALOR
+            if (!currentNode.right) {//VALIDA QUE NO EXISTA NODO LADO DERECHO "!"
+
               currentNode.right = newNode;
               return this;
             }
