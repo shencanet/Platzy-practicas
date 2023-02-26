8d1a8b27abf89541b602ebe6ecfba8ff69634487
shen <santi.dionis@gmail.com>
2023-02-26 21:02:05 +0100

    ADD: arrays

---
+++ Estructura Datos/Array/01-ForEach.js
@@ -0,0 +1,21 @@
+const letters = ['a', 'b', 'c'];
+
+//array.forEach(function(element, index, array))
+letters.forEach(element => console.log('foreach',element));
+//vs
+for(let index = 0; index < letters.length; index++){
+    const element = letters[index];
+    console.log('for',element);
+}
+
+const products = [
+    { title: 'Burger', price: 121 },
+    { title: 'Pizza', price: 20 },
+    { title: 'Soda', price: 5 },
+  ]
+  //<div id="app"></div> html
+  const app = document.getElementById('app')
+  
+  products.forEach(product => {
+    app.innerHTML += `<li>${product.title} -$ ${product.price}</li>`
+  })
--- Estructura Datos/Array/ForEach.js
+++
@@ -1,9 +0,0 @@
-const letters = ['a', 'b', 'c'];
-
-
-letters.forEach(element => console.log(element));
-//vs
-for(let index = 0; index < letters.length; index++){
-    const element = letters[index];
-    console.log(element);
-}
