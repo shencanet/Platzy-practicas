d831044b1141de23a201d5001c72d4413ff5b0df
shen <santi.dionis@gmail.com>
2023-02-27 20:43:42 +0100

    ADD: Reduce

--- Estructura Datos/Array/05-reduce.js
+++ Estructura Datos/Array/05-reduce.js
@@ -1,18 +1,18 @@
 /*
 El método reduce es inmutable y consiste retornar un solo valor del array iterado a partir de una función (callback) que indica de qué manera se itera cada elemento para reducirlo. */
 
-let reducedValue = array.reduce(function(), initialValue)
+let reducedValue0 = array.reduce(function(), initialValue)
 
 let reducedValue1 = array.reduce(
     function(acumulator,element, index, array), 
     valorInicial
 )
 
-const numbers = [5,4,8,6,2]
+const numbers1 = [5,4,8,6,2]
 let suma = 0
 
 for(let i = 0; i<numbers.length; i++){
-  const number = numbers[i]
+  const number = numbers1[i]
   suma = suma + number**2
 }
 
@@ -23,3 +23,15 @@ const numbers = [5,4,8,6,2]
 const reducedValue = numbers.reduce((suma, number) => suma + number**2)
 
 console.log(reducedValue) // 125
+
+const totals = [1,2,3,4];
+
+let sum = 0;
+for (let index = 0; index < totals.length; index++) {
+    const element = totals[index];
+    sum = sum + element;
+}
+console.log(sum);
+
+const rta = totals.reduce((sum, element) => sum + element, 0)
+console.log('rta', rta);
