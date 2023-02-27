1af5b8381a54b3fe790c060f0fc394a98bf3204b
shen <santi.dionis@gmail.com>
2023-02-27 21:08:33 +0100

    ADD: Reduce

--- Estructura Datos/Array/06-reduce-reloaded.js
+++ Estructura Datos/Array/06-reduce-reloaded.js
@@ -50,3 +50,28 @@ const rta1 = data
 }, {});
 
 console.log(rta1);
+
+//reto  agrupar por rangos
+
+const list = [];
+
+for (let i=0; i<30; i++){
+  list.push(Math.floor(Math.random()*11));
+
+  
+}
+const rst = list.reduce((obj, value)=>{
+  if (value < 5) obj["1-4"]+= 1;
+  else if (value <9) obj["5-8"] += 1;
+  else obj["9-10"] += 1;
+  return obj
+}, {
+
+  "1-4": 0,
+  "5-8": 0,
+  "9-10": 0,
+}
+
+)
+
+console.log(rst)
