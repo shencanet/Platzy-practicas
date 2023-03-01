fa058900b58c6c971ecfadc8027a7e56340d56de
shen <santi.dionis@gmail.com>
2023-03-01 19:29:06 +0100

    ADD: find

--- Estructura Datos/Array/includes.js
+++ Estructura Datos/Array/includes.js
@@ -12,3 +12,21 @@ for (let index = 0; index < pets.length; index++) {
 const rta = pets.includes('dog')
 console.log('for', includeInArray);
 console.log('includes', rta);
+
+/*
+1
+2
+3
+4
+
+Intentos
+
+Tu reto es retornar un array solo con las palabras 
+que cumplan con la condición de tener un término de 
+búsqueda dado.*/
+function solution(words, query) {
+	const result = words.filter(word => {
+		return word.includes(query);
+	});
+	return result;
+};
